dd09a8aaed0336ed53caebbdee6516e910c96573
HumaNisar <humajamil200@gmail.com>
2025-08-25 06:51:36 +0000

    Mon, Aug 25, 2025, 6:51 AM +00:00

--- shows.py
+++ shows.py
@@ -18,7 +18,7 @@ def main():
               print (show.strip())
               cleaned_shows.append (show.strip().title())
 
-             print(''.join(cleaned_shows))
+              print(' '.join(cleaned_shows))
 
 
               print(cleaned_shows)
