bb229bcdfe60e2db0a341b9ea716d3bcb4ac475a
HumaNisar <humajamil200@gmail.com>
2025-08-25 04:44:21 +0000

    Mon, Aug 25, 2025, 4:44 AM +00:00

--- clock.py
+++ clock.py
@@ -9,4 +9,13 @@ print (time + " in " + timezone)
 time = 1741604400
 time = time + 60
 
+def area(length, width)
+        print(str(length*width) + "square feet")
+
+
+        def main( ):
+                area(50,20)
+
+
+main()
 print(time)
