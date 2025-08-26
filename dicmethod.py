d5540cdd479562dc680aeda817ccf9ae982ef332
HumaNisar <humajamil200@gmail.com>
2025-08-26 09:22:23 +0000

    Tue, Aug 26, 2025, 9:22 AM +00:00

--- dicmethod.py
+++ dicmethod.py
@@ -2,4 +2,9 @@ WORDS = {"Pair" : 4, "Hair" : 4, "Chair" : 5}
 
 
 def main():
-    pr
+    print("welcome to the spelling Bee")
+    print("your letters are: A I P C R H G")
+
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} words left!")
+        guess = input ("Guess a word")
